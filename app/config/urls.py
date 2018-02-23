dfbee0ec1abf7157a4aeaf30751b8a73c897218a
kahee <yuygh131@gmail.com>
2018-02-23 15:41:14 +0900

    signup_view()동작 구현 및 base.html 수정

--- app/config/urls.py
+++ app/config/urls.py
@@ -20,11 +20,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from config import views
-from members.views import login_view
+from members.views import login_view, logout_view, signup_view
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('signup/', signup_view, name='signup'),
     path('admin/', admin.site.urls),
     path('artist/', include('artist.urls')),
     path('album/', include('album.urls')),
--- app/members/views.py
+++ app/members/views.py
@@ -1,8 +1,13 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.shortcuts import render, redirect
 
+# user모델 클래스가져올때는 꼭 이렇게 할 것
+User = get_user_model()
 
 # Create your views here.
+from members.models import User
+
+
 def login_view(request):
     # POST요청일때는
     # authentication -> login 후 'index"로 redirect
@@ -23,3 +28,40 @@ def login_view(request):
             return redirect('index')
 
     return render(request, 'members/login_view.html')
+
+
+def logout_view(request):
+    # /logout/
+    #  GET/POST요청 둘다 상관없음
+    logout(request)
+    return redirect('index')
+
+
+def signup_view(request):
+    # /signup/
+    # username, password , password2가 전달되었다는 가정
+    # username이 중복되는지 검사, 존재하지 않으면 유저 생성후 index로 이동
+    # 이외의 경우, 다시 회원가입화면으로
+
+    context = {
+        'errors': []
+    }
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        is_valid = True
+        if User.objects.filter(username=username).exists():
+            context['errors'].append('Username already exists')
+            is_valid = False
+        if password != password2:
+            context['errors'].append('Password and Password2 is not equal')
+            is_valid = False
+
+        if is_valid:
+            User.objects.create(username=username, password=password)
+            return render(request, 'index.html')
+
+    return render(request, 'members/signup.html', context)
