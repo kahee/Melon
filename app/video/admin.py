e65f5eea7cce4aab8654e6da5035824b50712d3f
kahee <yuygh131@gmail.com>
2018-02-28 00:35:06 +0900

    video migrate

--- app/video/admin.py
+++ app/video/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-admin.site.register('video')
+from video.models import Video
+
+admin.site.register(Video)
--- app/video/models.py
+++ app/video/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from artist.models import Artist
+
+
 class Video(models.Model):
-    pass
-# 제목, 비디오id , url
+    video_id = models.CharField('videoID', max_length=200, blank=True)
+    title = models.CharField('제목', max_length=200, blank=True)
+    img_url = models.ImageField('썸네일', upload_to='artist', blank=True)
+    artists = models.ManyToManyField(
+        Artist,
+        verbose_name='아티스트 목록',
+        blank=True,
+    )
+
+    def __str__(self):
+        return f'{self.video_id} - {self.title}'
