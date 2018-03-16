c47a24040dcc4b5c156e2e746029d0c333af8323
kahee <yuygh131@gmail.com>
2018-03-16 12:11:17 +0900

    APIView로 api 구현

--- app/artist/urls/apis.py
+++ app/artist/urls/apis.py
@@ -3,5 +3,6 @@ from .. import apis
 
 app_name = 'artist'
 urlpatterns = [
-    path('', apis.artist_list, name='artist-list')
+    path('', apis.artist_list, name='artist-list'),
+    path('drf/', apis.ArtistListView.as_view(), name='artist-list2'),
 ]
